b507cc7a0e7d8b39ada393cfc6408f364e639b53
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-12 11:39:17 -0300

    Iterando com a diretiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  id = 0;
   nome = 'Gabriel';
   adicionado = false;
+  funcionarios = [];
 
   adicionar(){
     console.log(`Adicionando ${this.nome}`)
    this.adicionado = true;
+
+   this.funcionarios.push({
+    id: ++this.id,
+    nome:  this.nome
+   });
   }
 
 }
